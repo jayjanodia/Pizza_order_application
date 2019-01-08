1a830cc6ea733e472a7d110408d8b3a521f95338
jayjanodia <jay.janodia@gmail.com>
2019-01-08 07:05:57 +0530

    Incomplete

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 var c = function(pos) {
     var lat = pos.coords.latitude,
         long = pos.coords.longitude,
