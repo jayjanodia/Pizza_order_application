b36bbb23243b6fce7f502d8b2e4b8d6735aaf920
Jay Janodia <jay.janodia@gmail.com>
2019-02-10 08:51:10 +0530

    FrontEnd Completed

--- script.js
+++ script.js
@@ -1,54 +1,93 @@
-<<<<<<< HEAD
-// Get the modal
-var modal = document.getElementById('id01');
+//Application: Pizza Order
+
+
+
+var modal = document.getElementById("id01");
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    window.onclick = function(event) {
+        if (event.target == modal) {
         modal.style.display = "none";
-    }
+        }
+	}
+	
+function ValidateEmail(inputText)
+{
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(inputText.value.match(mailformat))
+        {	
+        	alert("LOGIN SUCCESSFUL");
+	        return true;
+        }
+        else
+        {
+	        alert("You have entered an invalid email address!");
+	        return false;
+        }
+}
+ function myAlert(){
+ 		
+            var name = document.getElementById("Name").value;
+            var mobile = document.getElementById("Phone").value;
+            var email = document.getElementById("email").value;
+            var password = document.getElementById("psw").value;
+            var repeat = document.getElementById("psw-repeat").value;
+            var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            var message = "SIGN UP SUCCESSFUL";
+			var messageName = null;
+			var mobileVal = null;
+            var message1 = null;
+            var message2 = null;
+
+            
+            /*if(name == "") {
+                name.style.border = "1px solid red";
+                name.focus();
+                return false;
+            }*/
+			if(name.length < 8) {
+				messageName = "Please enter a username greater than 8 characters\n";
+			}
+			if(mobile.length !=10) {
+				mobileVal = "Invalid mobile number entered, please enter correct mobile number\n";
+			}
+            if(!email.match(mailformat))
+            {
+                message1 = "You have entered an invalid email address!\n";
+            }
+            if(password != repeat) 
+                message2="Entered Passwords do not match\n";            
+            else {
+                    if(password.length <8)
+                        message2="Password should be of 8 characters or more\n";      
+            }
+			if(messageName != null || mobileVal != null || message1 != null || message2 != null)
+					message = messageName + mobileVal + message1 + message2;
+            alert(message);
 }
 
-/*var modal1 = document.getElementById('id02');
 
-window.onclick = function(event) {
-    if(event.target == modal1) {
-        modal1.style.display = "none";
-    }
+
+/*window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("close");
+var sticky = close.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    close.classList.add("sticky")
+  } else {
+    close.classList.remove("sticky");
+  }
+}
+var c = function(pos) {
+    var lat = pos.coords.latitude,
+        long = pos.coords.longitude,
+        coords = lat +','+ long;
+    document.getElementById('google_map').setAttribute('src','https://maps.google.co.in?q=' + coords +'&z=60&output=embed');
+}
+
+document.getElementById('get_location').onClick = function() {
+    navigator.geolocation.getCurrentPosition(c);
+    return false; 
 }*/
-
-var c = function(pos) {
-    var lat = pos.coords.latitude,
-        long = pos.coords.longitude,
-        coords = lat +','+ long;
-    document.getElementById('google_map').setAttribute('src','https://maps.google.co.in?q=' + coords +'&z=60&output=embed');
-}
-
-document.getElementById('get_location').onClick = function() {
-    navigator.geolocation.getCurrentPosition(c);
-    return false; 
-}
-
-=======
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-var c = function(pos) {
-    var lat = pos.coords.latitude,
-        long = pos.coords.longitude,
-        coords = lat +','+ long;
-    document.getElementById('google_map').setAttribute('src','https://maps.google.co.in?q=' + coords +'&z=60&output=embed');
-}
-
-document.getElementById('get_location').onClick = function() {
-    navigator.geolocation.getCurrentPosition(c);
-    return false; 
-}
->>>>>>> b0a8a9eb4d35056814dfda9d23e5657bdfdb61a1
